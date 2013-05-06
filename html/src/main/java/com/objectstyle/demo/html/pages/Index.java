d1f3e78be7f365d9cea33cda430711815c241bd0
andrus <andrus@objectstyle.org>
2013-05-06 08:48:31 +0300

    pagewrapper

--- html/src/main/java/com/objectstyle/demo/html/pages/Index.java
+++ html/src/main/java/com/objectstyle/demo/html/pages/Index.java
@@ -1,16 +1,14 @@
 package com.objectstyle.demo.html.pages;
 
 import org.apache.tapestry5.annotations.Persist;
-import org.apache.tapestry5.annotations.Property;
 
 public class Index {
 
-	@Property
 	@Persist
 	private int clickCounter;
 
 	public String getText() {
-		return "Hi! clicked " + clickCounter + " time(s)";
+		return "Hi! Clicked " + clickCounter + " time(s)";
 	}
 
 	public void onActionFromClick() {
