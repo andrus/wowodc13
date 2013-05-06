587df78b489a2ea766c03038fee78566e4e08950
Andrus Adamchik <andrus@objectstyle.com>
2013-05-06 08:48:31 +0300

    pagewrapper

---
+++ html/src/main/java/com/objectstyle/demo/html/components/PageWrapper.java
@@ -0,0 +1,11 @@
+package com.objectstyle.demo.html.components;
+
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Property;
+
+public class PageWrapper {
+
+	@Parameter
+	@Property
+	private String title;
+}
