2ada39af098d89076f4c089b1c298ac5d16ebf4e
andrus <andrus@objectstyle.org>
2013-05-06 08:28:18 +0300

    added clicker

--- html/src/main/java/com/objectstyle/demo/html/pages/Index.java
+++ html/src/main/java/com/objectstyle/demo/html/pages/Index.java
@@ -1,8 +1,19 @@
 package com.objectstyle.demo.html.pages;
 
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
+
 public class Index {
 
+	@Property
+	@Persist
+	private int clickCounter;
+
 	public String getText() {
-		return "Hi!";
+		return "Hi! clicked " + clickCounter + " time(s)";
+	}
+
+	public void onActionFromClick() {
+		clickCounter++;
 	}
 }
