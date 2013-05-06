f8fd019c7d7b5540a763e788beaa1b73da4162d9
andrus <andrus@objectstyle.org>
2013-05-06 08:17:50 +0300

    hello-world working

---
+++ html/src/main/java/com/objectstyle/demo/html/pages/Index.java
@@ -0,0 +1,8 @@
+package com.objectstyle.demo.html.pages;
+
+public class Index {
+
+	public String getText() {
+		return "Hi!";
+	}
+}
---
+++ html/src/main/java/com/objectstyle/demo/html/services/HtmlModule.java
@@ -0,0 +1,10 @@
+package com.objectstyle.demo.html.services;
+
+import org.apache.tapestry5.ioc.ServiceBinder;
+
+public class HtmlModule {
+
+	public static void bind(ServiceBinder binder) {
+
+	}
+}
