ec6285c588386861dd671d881c8028217dcc6fe7
andrus <andrus@objectstyle.org>
2013-05-11 10:45:11 +0300

    logging setup

--- html/src/main/java/com/objectstyle/demo/html/pages/News.java
+++ html/src/main/java/com/objectstyle/demo/html/pages/News.java
@@ -1,5 +1,25 @@
 package com.objectstyle.demo.html.pages;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.cayenne.query.SelectQuery;
+import org.apache.tapestry5.annotations.Property;
+
+import com.objectstyle.demo.cayenne.Article;
+import com.objectstyle.demo.html.services.cayenne.ICayenneService;
+
 public class News {
 
+	@Property
+	private Article article;
+
+	@Inject
+	private ICayenneService cayenneService;
+
+	public List<Article> getNewsList() {
+		SelectQuery<Article> query = new SelectQuery<Article>(Article.class);
+		return cayenneService.sharedContext().select(query);
+	}
 }
