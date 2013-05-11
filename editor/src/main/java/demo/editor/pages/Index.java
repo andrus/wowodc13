05693f96b9185f1a1379842e39b91507bf3d909b
andrus <andrus@objectstyle.org>
2013-05-11 18:57:35 +0300

    adding editor components

---
+++ editor/src/main/java/demo/editor/pages/Index.java
@@ -0,0 +1,5 @@
+package demo.editor.pages;
+
+public class Index {
+
+}
---
+++ editor/src/main/java/demo/editor/services/EditorModule.java
@@ -0,0 +1,10 @@
+package demo.editor.services;
+
+import org.apache.tapestry5.ioc.annotations.SubModule;
+
+import demo.services.ServicesModule;
+
+@SubModule(ServicesModule.class)
+public class EditorModule {
+
+}
