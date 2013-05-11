9e09d462f7bdb4a626425c5f64db71299b04e67f
andrus <andrus@objectstyle.org>
2013-05-11 16:27:29 +0300

    serialization wrapper

---
+++ html/src/main/java/demo/rest/model/JaxbArticles.java
@@ -0,0 +1,19 @@
+package demo.rest.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "news")
+public class JaxbArticles {
+
+	@XmlElement(name = "article")
+	public List<JaxbArticle> articles = new ArrayList<JaxbArticle>();
+
+	public void addArticle(JaxbArticle article) {
+		articles.add(article);
+	}
+
+}
--- html/src/main/java/demo/rest/resources/NewsResource.java
+++ html/src/main/java/demo/rest/resources/NewsResource.java
@@ -1,10 +1,15 @@
 package demo.rest.resources;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import demo.cayenne.Article;
 import demo.html.services.news.INewsService;
+import demo.rest.model.JaxbArticle;
+import demo.rest.model.JaxbArticles;
 
 @Path("news")
 public class NewsResource {
@@ -14,6 +19,13 @@ public class NewsResource {
 
 	@GET
 	public Object get() {
-		return newsService.getRecentNews().toString();
+		List<Article> articles = newsService.getRecentNews();
+
+		JaxbArticles response = new JaxbArticles();
+		for (Article a : articles) {
+			response.addArticle(new JaxbArticle(a));
+		}
+
+		return response;
 	}
 }
