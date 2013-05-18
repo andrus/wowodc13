0845d625e0b5add74e75d65beee8508500f002ee
andrus <andrus@objectstyle.org>
2013-05-18 11:33:10 +0300

    news list

--- editor/src/main/java/demo/editor/pages/Index.java
+++ editor/src/main/java/demo/editor/pages/Index.java
@@ -1,5 +1,39 @@
 package demo.editor.pages;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.cayenne.Cayenne;
+import org.apache.tapestry5.annotations.Property;
+
+import demo.cayenne.Article;
+import demo.services.news.INewsService;
+
 public class Index {
+
+	@Property
+	private Article article;
+
+	@Inject
+	private INewsService newsService;
+
+	public List<Article> getNewsList() {
+		return newsService.getRecentNews();
+	}
+
+	public Format getPublishDateFormat() {
+		return new SimpleDateFormat("yyyy-MM-dd HH:mm");
+	}
+
+	public int getArticleId() {
+		return Cayenne.intPKForObject(article);
+	}
+
+	public Object onActionFromArticleLink(int articleId) {
+		return Index.class;
+	}
 
 }
