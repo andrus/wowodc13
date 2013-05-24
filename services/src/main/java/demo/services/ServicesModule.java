8e037e59a1e092d08de4168572ae85f76206cd15
Andrus Adamchik <andrus@objectstyle.com>
2013-05-24 14:55:53 +0300

    ActiveMQ JMS cluster service

--- services/src/main/java/demo/services/ServicesModule.java
+++ services/src/main/java/demo/services/ServicesModule.java
@@ -4,15 +4,19 @@ import org.apache.tapestry5.ioc.ServiceBinder;
 
 import demo.services.cayenne.CayenneService;
 import demo.services.cayenne.ICayenneService;
+import demo.services.cluster.IClusterService;
+import demo.services.cluster.JmsClusterService;
 import demo.services.domain.IDomainService;
 import demo.services.domain.VhostDomainService;
 import demo.services.news.INewsService;
 import demo.services.news.NewsService;
 
 public class ServicesModule {
-	
+
 	public static void bind(ServiceBinder binder) {
 		binder.bind(ICayenneService.class, CayenneService.class);
+		binder.bind(IClusterService.class, JmsClusterService.class);
+
 		binder.bind(IDomainService.class, VhostDomainService.class);
 		binder.bind(INewsService.class, NewsService.class);
 	}
